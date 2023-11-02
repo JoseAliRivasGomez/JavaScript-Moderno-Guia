fec379e2bae44fd5bb2d882a4f5968972f9bcd1e
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2023-11-02 15:49:51 -0600

    cotizadores react y vie

---
+++ 56-cotizador-prestamos-vue/src/components/Button.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const props = defineProps({
+        operador: String
+    });
+    const hola = "hola";
+</script>
+
+<template>
+    <button
+        type="button"
+        class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+        @click="$emit('fn', hola)"
+    >{{ operador }}</button>
+</template>
